18f4aea05bbe7a4c659e85595e5351627ac2f112
Mike <129897918+RedParrotBerkeley@users.noreply.github.com>
2025-03-18 15:25:21 -0800

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.routers import upload
 
 app = FastAPI()
+
+app.include_router(upload.router)
 
 @app.get("/ping")
 def health_check():
     return {"message": "API is running"}
+
